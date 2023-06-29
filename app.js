02fc4ae4eef316790b626ef840a7072adf35e28b
franco427 <calatayfranco719@gmail.com>
2023-06-29 16:47:09 -0300

    archivos para subir

---
+++ app.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const pelicula = require("./peliculas");
+
+let mostrar;
+function mostrarDetalles(listado){
+    for (let i=0;i<listado.length;i++){
+    console.log((i+1)+"."+"calificacion :"+listado[i].calificacion,"-premios: "+listado[i].premios,"-duracion: "+listado[i].duracion,"-precio: "+listado[i].precio);
+    }
+}
+mostrarDetalles(pelicula);
+
+//const mostrar2=require("mensaje.txt")
+const leer = fs.readFileSync("mensaje.txt","utf8")
+console.log(leer);
---
+++ peliculas.js
@@ -0,0 +1,19 @@
+let peliculas=[
+    {
+    id:"shreak",
+    calificacion:8.9,
+    premios:"oscar",
+    duracion:"150mn",
+    precio:75000,
+    genero:"animacion"
+},
+{ 
+    id:"spiderman 2",
+    calificacion:9.5,
+    premios:"oscar",
+    duracion:"270mn",
+    precio:619,
+    genero:"accion"
+}
+];
+module.exports = peliculas;
